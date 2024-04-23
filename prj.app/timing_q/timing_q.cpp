68644673f5cabfc32df2d4a562930a6a004d315d
prestcote <vasilisaschvarz@gmail.com>
2024-04-24 00:40:56 +0300

    time tests // i dont know what were supposed to do

--- prj.app/timing_q/timing_q.cpp
+++ prj.app/timing_q/timing_q.cpp
@@ -1,14 +1,16 @@
-#include <queuelst/queuelst.hpp>
-#include <queuearr/queuearr.hpp>
+#include <queuelstt/queuelstt.hpp>
+#include <queuearrt/queuearrt.hpp>
+#include <complex/complex.hpp>
 
 #include <chrono>
 #include <iostream>
 
 int QueueArrPush(int elements) {
-  QueueArr d;
+  QueueArrT<Complex> d;
   const auto start_arr{ std::chrono::high_resolution_clock::now() };
   for (int i = 0; i < elements; i++) {
     d.Push(Complex(i, 0));
+    //d.Push(i);
   }
   const auto end_arr{ std::chrono::high_resolution_clock::now() };
   const std::chrono::nanoseconds elapsed_seconds_arr{ end_arr - start_arr };
@@ -16,11 +18,55 @@ int QueueArrPush(int elements) {
   return res;
 }
 
-int QueueLstPush(int elements) {
-  QueueLst p;
-  const auto start_lst{ std::chrono::high_resolution_clock::now() };
-  for (int i = 0; i < 10; i++) {
+int QueueArrPop(int elements) {
+  QueueArrT<Complex> d;
+  for (int i = 0; i < elements; i++) {
+    d.Push(Complex(i, 0));
+    //d.Push(i);
+  }
+  const auto start_arr{ std::chrono::high_resolution_clock::now() };
+  while (!d.IsEmpty()) {
+    d.Pop();
+  }
+  const auto end_arr{ std::chrono::high_resolution_clock::now() };
+  const std::chrono::nanoseconds elapsed_seconds_arr{ end_arr - start_arr };
+  int res = elapsed_seconds_arr.count();
+  return res;
+}
+
+int QueueArrCtorCopy(int elements) {
+  QueueArrT<Complex> d;
+  for (int i = 0; i < elements; i++) {
+    d.Push(Complex(i, 0));
+  }
+  const auto start_copyarr{ std::chrono::high_resolution_clock::now() };
+  QueueArrT<Complex> copy(d);
+  const auto end_copyarr{ std::chrono::high_resolution_clock::now() };
+  const std::chrono::nanoseconds timer_copyctor_arr{ end_copyarr - start_copyarr };
+  int res = timer_copyctor_arr.count();
+  return res;
+}
+
+int QueueArrCopy(int elements) {
+  QueueArrT<Complex> p;
+  for (int i = 0; i < elements; i++) {
     p.Push(Complex(i, 0));
+  }
+  QueueArrT<Complex> d;
+  const auto start_arr{ std::chrono::high_resolution_clock::now() };
+  d = p;
+  const auto end_arr{ std::chrono::high_resolution_clock::now() };
+  const std::chrono::nanoseconds seconds{ end_arr - start_arr };
+  int res = seconds.count();
+  return res;
+}
+
+int QueueLstPush(int elements) {
+  QueueLstT<Complex> p;
+  const auto start_lst{ std::chrono::high_resolution_clock::now() };
+  for (int i = 0; i < elements; i++) {
+    p.Push(Complex(i, 0));
+    //p.Push(i);
   }
   const auto end_lst{ std::chrono::high_resolution_clock::now() };
   const std::chrono::nanoseconds elapsed_seconds{ end_lst - start_lst };
@@ -28,27 +74,101 @@ int QueueLstPush(int elements) {
   return res;
 }
 
+int QueueLstPop(int elements) {
+  QueueLstT<Complex> d;
+  for (int i = 0; i < elements; i++) {
+    d.Push(Complex(i, 0));
+  }
+  const auto start_arr{ std::chrono::high_resolution_clock::now() };
+  while (!d.IsEmpty()) {
+    d.Pop();
+  }
+  const auto end_arr{ std::chrono::high_resolution_clock::now() };
+  const std::chrono::nanoseconds elapsed_seconds_arr{ end_arr - start_arr };
+  int res = elapsed_seconds_arr.count();
+  return res;
+}
+
+int QueueLstCtorCopy(int elements) {
+  QueueLstT<Complex> d;
+  for (int i = 0; i < elements; i++) {
+    d.Push(Complex(i, 0));
+  }
+  const auto start_copyarr{ std::chrono::high_resolution_clock::now() };
+  QueueLstT<Complex> copy(d);
+  const auto end_copyarr{ std::chrono::high_resolution_clock::now() };
+  const std::chrono::nanoseconds timer_copyctor_arr{ end_copyarr - start_copyarr };
+  int res = timer_copyctor_arr.count();
+  return res;
+}
+
+void PushTest(int elements) {
+  std::cout << "number of elements: " << elements << std::endl;
+  int av_arr = 0;
+  for (int i = 0; i < 10; i++) {
+    av_arr += QueueArrPush(elements);
+  }
+  std::cout << "arr time: " << av_arr / 10 << std::endl;
+  int av_lst = 0;
+  for (int i = 0; i < 10; i++) {
+    av_lst += QueueLstPush(elements);
+  }
+  std::cout << "lst time: " << av_lst / 10 << std::endl << std::endl;
+}
+
+
+void PopTest(int elements) {
+  std::cout << "number of elements: " << elements << std::endl;
+  int av_arr = 0;
+  for (int i = 0; i < 10; i++) {
+    av_arr += QueueArrPop(elements);
+  }
+  std::cout << "arr time: " << av_arr / 10 << std::endl;
+  int av_lst = 0;
+  for (int i = 0; i < 10; i++) {
+    av_lst += QueueLstPop(elements);
+  }
+  std::cout << "lst time: " << av_lst / 10 << std::endl << std::endl;
+}
+
+void CopyCtorTest(int elements) {
+  std::cout << "number of elements copied: " << elements << std::endl;
+  int av_arr = 0;
+  for (int i = 0; i < 10; i++) {
+    av_arr += QueueArrCtorCopy(elements);
+  }
+  std::cout << "arr time: " << av_arr / 10 << std::endl;
+  int av_lst = 0;
+  for (int i = 0; i < 10; i++) {
+    av_lst += QueueLstCtorCopy(elements);
+  }
+  std::cout << "lst time: " << av_lst / 10 << std::endl << std::endl;
+}
+
 int main() {
-  std::cout << "PUSH time test" << std::endl << std::endl;
 
-  std::cout << "number of elements: 2" << std::endl;
-  std::cout << "arr time: " << QueueArrPush(2) << std::endl;
-  std::cout << "lst time: " << QueueLstPush(2) << std::endl << std::endl;
+  std::cout << "PUSH time test; average of 10 measurements" << std::endl << std::endl;
+  PushTest(1);
+  PushTest(2);
+  PushTest(10);
+  PushTest(100);
+  PushTest(1000);
+  PushTest(10000);
 
-  std::cout << "number of elements: 10" << std::endl;
-  std::cout << "arr time: " << QueueArrPush(10) << std::endl;
-  std::cout << "lst time: " << QueueLstPush(10) << std::endl << std::endl;
-
-  std::cout << "number of elements: 100" << std::endl;
-  std::cout << "arr time: " << QueueArrPush(100) << std::endl;
-  std::cout << "lst time: " << QueueLstPush(100) << std::endl << std::endl;
-
-  std::cout << "number of elements: 10000" << std::endl;
-  std::cout << "arr time: " << QueueArrPush(1000) << std::endl;
-  std::cout << "lst time: " << QueueLstPush(1000) << std::endl << std::endl;
-
-  std::cout << "number of elements: 10000" << std::endl;
-  std::cout << "arr time: " << QueueArrPush(10000) << std::endl;
-  std::cout << "lst time: " << QueueLstPush(10000) << std::endl;
+  std::cout << "POP time test; average of 10 measurements" << std::endl << std::endl;
+  PopTest(1);
+  PopTest(2);
+  PopTest(10);
+  PopTest(100);
+  PopTest(1000);
+  PopTest(10000);
   
+  std::cout << "COPY CTOR time test; average of 10 measurements" << std::endl << std::endl;
+  CopyCtorTest(0);
+  CopyCtorTest(1);
+  CopyCtorTest(2);
+  CopyCtorTest(10);
+  CopyCtorTest(100);
+  CopyCtorTest(1000);
+  CopyCtorTest(10000);
 }
